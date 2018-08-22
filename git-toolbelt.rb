da508336d309640161f7cde9601ae491721309f9
Vincent Driessen <me@nvie.com>
2018-08-22 12:44:47 +0200

    Update to v1.1.2

--- git-toolbelt.rb
+++ git-toolbelt.rb
@@ -1,8 +1,8 @@
 class GitToolbelt < Formula
   desc "Suite of useful Git commands"
   homepage "https://github.com/nvie/git-toolbelt#readme"
-  url "https://github.com/nvie/git-toolbelt/archive/v1.1.1.tar.gz"
-  sha256 "802b6be3121315ee818c89a346747b11526096f5d09146af3d802d002ec8ce84"
+  url "https://github.com/nvie/git-toolbelt/archive/v1.1.2.tar.gz"
+  sha256 "b2ce32634af37199b8204bda890aa0f4382bb6efd5979f39614da098fbfdc58f"
 
   def install
     bin.install "git-active-branches"
