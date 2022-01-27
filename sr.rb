669afe59ef1fd930dad91232b20fbb0674e7e96e
Vincent Driessen <me@nvie.com>
2022-01-27 22:44:09 +0100

    Fix sha

--- sr.rb
+++ sr.rb
@@ -2,7 +2,7 @@ class Sr < Formula
   desc "A simple mass search & replace tool"
   homepage "https://github.com/nvie/sr#readme"
   url "https://github.com/nvie/sr/releases/download/v0.1.0/sr-0.1.0-mac.tar.gz"
-  sha256 "4c8a0529bebbcf7e84252c497b72697496d6a3d7a89efad8e8a9015640181a71"
+  sha256 "5737e30742a32999f7f86f1f39e2005aec2f3529c3b32fe776f552fc2bf75971"
   version "0.1.0"
 
   def install
