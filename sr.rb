887300de50ba2f9e6e4dadbffd9f795a007396e8
Vincent Driessen <me@nvie.com>
2022-06-08 11:59:37 +0200

    Publish sr v0.1.1

--- sr.rb
+++ sr.rb
@@ -1,9 +1,9 @@
 class Sr < Formula
   desc "A simple mass search & replace tool"
   homepage "https://github.com/nvie/sr#readme"
-  url "https://github.com/nvie/sr/releases/download/v0.1.0/sr-0.1.0-mac.tar.gz"
-  sha256 "5737e30742a32999f7f86f1f39e2005aec2f3529c3b32fe776f552fc2bf75971"
-  version "0.1.0"
+  url "https://github.com/nvie/sr/releases/download/v0.1.1/sr-0.1.1-mac.tar.gz"
+  sha256 "febce8f899bd199f7fb05954dd8f6a0a667074c86a242b6a6d5cac86ffa7f889"
+  version "0.1.1"
 
   def install
     bin.install "sr"
