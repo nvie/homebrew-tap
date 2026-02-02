bf97bafccd7bbf85bc9617f7a7d8dc8ff2b93a90
Vincent Driessen <me@nvie.com>
2026-02-02 11:59:33 +0100

    Add fish 3.7.1 formula (as backup, before upgrading to 4.x)

---
+++ Formula/fish@3.7.1.rb
@@ -0,0 +1,47 @@
+class FishAT371 < Formula
+  desc "User-friendly command-line shell for UNIX-like operating systems"
+  homepage "https://fishshell.com"
+  url "https://github.com/fish-shell/fish-shell/releases/download/3.7.1/fish-3.7.1.tar.xz"
+  sha256 "614c9f5643cd0799df391395fa6bbc3649427bb839722ce3b114d3bbc1a3b250"
+  license "GPL-2.0-only"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  pour_bottle? only_if: :default_prefix
+
+  head do
+    url "https://github.com/fish-shell/fish-shell.git", branch: "master"
+
+    depends_on "rust" => :build
+    depends_on "sphinx-doc" => :build
+  end
+
+  depends_on "cmake" => :build
+  # Apple ncurses (5.4) is 15+ years old and
+  # has poor support for modern terminals
+  depends_on "ncurses"
+  depends_on "pcre2"
+
+  def install
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args,
+                    "-DCMAKE_INSTALL_SYSCONFDIR=#{etc}",
+                    "-Dextra_functionsdir=#{HOMEBREW_PREFIX}/share/fish/vendor_functions.d",
+                    "-Dextra_completionsdir=#{HOMEBREW_PREFIX}/share/fish/vendor_completions.d",
+                    "-Dextra_confdir=#{HOMEBREW_PREFIX}/share/fish/vendor_conf.d"
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  def post_install
+    (pkgshare/"vendor_functions.d").mkpath
+    (pkgshare/"vendor_completions.d").mkpath
+    (pkgshare/"vendor_conf.d").mkpath
+  end
+
+  test do
+    system bin/"fish", "-c", "echo"
+  end
+end
