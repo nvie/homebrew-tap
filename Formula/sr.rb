7d8f2b82343aaed02153276d0f23425768abdaba
Vincent Driessen <me@nvie.com>
2026-09-04 12:50:50 +0200

    Bump `sr` formula to v0.2.0

--- Formula/sr.rb
+++ Formula/sr.rb
@@ -1,9 +1,9 @@
 class Sr < Formula
   desc "A simple mass search & replace tool"
   homepage "https://github.com/nvie/sr#readme"
-  url "https://github.com/nvie/sr/releases/download/v0.1.1/sr-0.1.1-mac.tar.gz"
-  sha256 "febce8f899bd199f7fb05954dd8f6a0a667074c86a242b6a6d5cac86ffa7f889"
-  version "0.1.1"
+  url "https://github.com/nvie/sr/releases/download/v0.2.0/sr-0.2.0-mac.tar.gz"
+  sha256 "22ef2132836bc982e1739a9a691e1f14ad40f59a6ad0082c61426a184d4bcde1"
+  version "0.2.0"
 
   def install
     bin.install "sr"
